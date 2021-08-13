2b46208c677f3b43044c5dd63d2d7b27c1289ddd
leoklems <leoklems@yahoo.com>
2021-08-13 06:06:31 +0100

    db settings update

--- xzero/settings.py
+++ xzero/settings.py
@@ -102,11 +102,11 @@ DATABASES = {
         'NAME': 'xzero',
         'USER': 'root',
         'PASSWORD': '',
-        'HOST': 'localhost',
+        'HOST': '127.0.0.1',
         'PORT': '3306',
         'OPTIONS': {
-                    'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-                    'charset': 'utf8mb4',
+                    # 'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+                    # 'charset': 'utf8mb4',
         }
     }
 }
